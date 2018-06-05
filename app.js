d1b92b37321cd03a277538b7e43900a15ff8be7d
Amarjeet Singh <33446077+amarjeet045@users.noreply.github.com>
2018-06-05 13:41:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,3 +15,13 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.getElementById('dice-2').style.display = 'block';
         document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
         document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+
+        //3. Update the round score IF the rolled number was NOT a 1
+        if (dice1 !== 1 && dice2 !== 1) {
+            //Add score
+            roundScore += dice1 + dice2;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //Next player
+            nextPlayer();
+        }
